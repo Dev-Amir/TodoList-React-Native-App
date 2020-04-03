a8b9558861bda9f6e8c241808c071ce0e239361a
Amir <dev.amirhemmati1382@gmail.com>
2020-04-03 15:31:45 +0430

    project 2

---
+++ 1/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+export default function App() {
+  const { 
+
+  } = styles;
+
+  return (
+    <View>
+       
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+});
